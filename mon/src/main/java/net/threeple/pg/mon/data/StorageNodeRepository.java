cbea1425ed59363013409c9844062655874ef17f
Wayne Hamilton <hamilton1861@icloud.com>
2017-11-02 21:45:31 +0800

    修复家目录下缺少集群数据文件导致的错误

--- mon/src/main/java/net/threeple/pg/mon/data/StorageNodeRepository.java
+++ mon/src/main/java/net/threeple/pg/mon/data/StorageNodeRepository.java
@@ -1,9 +1,10 @@
 package net.threeple.pg.mon.data;
 
+import java.io.IOException;
 import java.util.List;
 
 import net.threeple.pg.mon.model.StorageNode;
 
 public interface StorageNodeRepository {
-	public List<StorageNode> getAllStorageNode();
+	public List<StorageNode> getAllStorageNode() throws IOException;
 }
--- mon/src/main/java/net/threeple/pg/mon/data/StorageNodeRepositoryImpl.java
+++ mon/src/main/java/net/threeple/pg/mon/data/StorageNodeRepositoryImpl.java
@@ -1,7 +1,7 @@
 package net.threeple.pg.mon.data;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,58 +18,51 @@ public class StorageNodeRepositoryImpl implements StorageNodeRepository {
 	final Logger logger = LoggerFactory.getLogger(StorageNodeRepositoryImpl.class);
 	
 	@Override
-	public List<StorageNode> getAllStorageNode() {
+	public List<StorageNode> getAllStorageNode() throws IOException {
 		String path = FileUtils.joinPath(System.getProperty("user.home"), "pg-cluster.data");
+		File file = new File(path);
+		if(!file.exists()) {
+			file = new File("/var/local/photo-gallery/pg-cluster.data");
+			if(!file.exists()) {
+				throw new IOException("集群文件不存在");
+			}
+		}
 		Properties prope = new Properties();
 		List<StorageNode> list = null;
-		try {
-			prope.load(new FileInputStream(path));
-			list = new ArrayList<>();
+		prope.load(new FileInputStream(file));
+		list = new ArrayList<>();
+		
+		for(int i = 0; i < 2; i++) {
+			StorageNode sn = new StorageNode();
+			sn.setId(i);
+			String t = prope.getProperty("ns" + i);
+			int s = t.indexOf('<');
+			String address = t.substring(0, s);
+			sn.setAddress(address);
+			logger.debug("获得存储节点#{}的地址：{}", i, address);
 			
-			for(int i = 0; i < 2; i++) {
-				StorageNode sn = new StorageNode();
-				sn.setId(i);
-				String t = prope.getProperty("ns" + i);
-				int s = t.indexOf('<');
-				String address = t.substring(0, s);
-				sn.setAddress(address);
-				logger.debug("获得存储节点#{}的地址：{}", i, address);
-				
-				t = t.substring(s + 1);
-				
-				List<PlacementGroup> roomers = new ArrayList<>();
-				while((s = t.indexOf(',')) > 0) {
-					int id = Integer.parseInt(t.substring(0, s));
-					PlacementGroup pg = new PlacementGroup(id, sn);
-					roomers.add(pg);
-					t = t.substring(s + 1);
-					logger.debug("获得存储节点#{}的归置组#{}", i, id);
-				}
-				int id = Integer.parseInt(t);
+			t = t.substring(s + 1);
+			
+			List<PlacementGroup> roomers = new ArrayList<>();
+			while((s = t.indexOf(',')) > 0) {
+				int id = Integer.parseInt(t.substring(0, s));
 				PlacementGroup pg = new PlacementGroup(id, sn);
 				roomers.add(pg);
+				t = t.substring(s + 1);
 				logger.debug("获得存储节点#{}的归置组#{}", i, id);
-				sn.setRoomers(roomers);
-				
-				list.add(sn);
 			}
-		} catch (FileNotFoundException e) {
-			logger.error("没有找到集群数据文件");
-		} catch (IOException e) {
-			logger.error("读写集群数据时出错，错误信息：{}", e.getMessage());
+			int id = Integer.parseInt(t);
+			PlacementGroup pg = new PlacementGroup(id, sn);
+			roomers.add(pg);
+			logger.debug("获得存储节点#{}的归置组#{}", i, id);
+			sn.setRoomers(roomers);
+			
+			list.add(sn);
 		}
 		return list;
 	}
 
 	
 	public static void main(String[] args) {
-		StorageNodeRepositoryImpl r = new StorageNodeRepositoryImpl();
-		List<StorageNode> list = r.getAllStorageNode();
-		for(StorageNode sn : list) {
-			System.out.println(sn);
-			for(PlacementGroup pg : sn.getRoomers()) {
-				System.out.println(pg);
-			}
-		}
 	}
 }
