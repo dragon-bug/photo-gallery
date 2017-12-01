d73dbb7165327c88de0abfc4d5a0b5be0e5d9401
Wayne Hamilton <hamilton1861@icloud.com>
2017-12-01 11:03:07 +0800

    添加(admin):PhotoGalleryController类执行命令

---
+++ admin/src/main/java/net/threeple/pg/adm/PhotoGalleryController.java
@@ -0,0 +1,31 @@
+package net.threeple.pg.adm;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import net.threeple.pg.adm.resource.DisplayClusterView;
+
+
+public class PhotoGalleryController {
+
+	public static void main(String[] args) throws Exception {
+		Option getOpt = Option.builder("get").hasArg().argName("get").desc("Display one or many resources").build();
+		Options options = new Options();
+		options.addOption(getOpt);
+		
+		CommandLineParser parser = new DefaultParser();
+		
+		CommandLine cmd = parser.parse(options, args);
+		
+		if(cmd.hasOption("get")) {
+			String resource = cmd.getOptionValue("get");
+			if("cv".equals(resource)) {
+				DisplayClusterView dcv = new DisplayClusterView();
+				dcv.display();
+			}
+		}
+	}
+}
